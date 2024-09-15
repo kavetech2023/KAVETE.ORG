b386e0c59ff2f112ff04ffc5dbea985f47498f73
kavetech2023 <kavetech@gmail.com>
2024-09-15 12:11:39 +0300

    Refactor Navbar component to update navigation items and add smooth scrolling functionality

--- kavete/src/Components/Navbar.jsx
+++ kavete/src/Components/Navbar.jsx
@@ -21,11 +21,11 @@ const Navbar = () => {
         </div>
         <nav className='hidden md:block'>
           <ul ref={menuRef} className='flex flex-row space-x-4 p-4'>
-          <li><AnchorLink className='anchor-link' offset={50} href="#home"><p onClick={()=>setMenu("home")}>Home</p></AnchorLink>{menu==="home"?<span>_</span>:<></>}</li>
-            <li><AnchorLink className='anchor-link' offset={250} href="#work"><p onClick={()=>setMenu("about")}>How it Works</p></AnchorLink>{menu==="about"?<span>_</span>:<></>}</li>
-            <li><AnchorLink className='anchor-link' offset={100} href="#services"><p onClick={()=>setMenu("services")}>Services</p></AnchorLink>{menu==="services"?<span>_</span>:<></>}</li>
-            <li><AnchorLink className='anchor-link' offset={100} href="#testimonials"><p onClick={()=>setMenu("testimonials")}>Testimonials</p></AnchorLink>{menu==="work"?<span>_</span>:<></>}</li>
-            <li><AnchorLink className='anchor-link' offset={50} href="#contact"><p onClick={()=>setMenu("contact")}>Contact</p></AnchorLink>{menu==="contact"?<span>_</span>:<></>}</li>
+          <li><AnchorLink className='anchor-link' offset={50} href="#home"><p className={`${menu==="home"? "font-bold" :""}`} onClick={()=>setMenu("home")}>Home</p></AnchorLink></li>
+            <li><AnchorLink className='anchor-link' offset={250} href="#work"><p className={`${menu==="about"? "font-bold" :""}`} onClick={()=>setMenu("about")}>How it Works</p></AnchorLink></li>
+            <li><AnchorLink className='anchor-link' offset={100} href="#services"><p className={`${menu==="services"? "font-bold" :""}`} onClick={()=>setMenu("services")}>Services</p></AnchorLink></li>
+            <li><AnchorLink className='anchor-link' offset={80} href="#testimonials"><p  className={`${menu==="testimonials"? "font-bold" :""}`} onClick={()=>setMenu("testimonials")}>Testimonials</p></AnchorLink></li>
+            <li><AnchorLink className='anchor-link' offset={50} href="#contact"><p className={`${menu==="contact"? "font-bold" :""}`} onClick={()=>setMenu("contact")}>Contact</p></AnchorLink></li>
           </ul>
 
         </nav>
