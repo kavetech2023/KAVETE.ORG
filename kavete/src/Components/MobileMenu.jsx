03563f79ae6c35c33fc15d0c701457c2f2b562cb
kavetech2023 <kavetech@gmail.com>
2024-09-15 11:06:42 +0300

    Refactor MobileMenu component to update navigation items and add smooth scrolling functionality

--- kavete/src/Components/MobileMenu.jsx
+++ kavete/src/Components/MobileMenu.jsx
@@ -1,31 +1,11 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import { Menu, X } from "lucide-react";
-
-const navItems = [
-{
-    title: "About US",
-    url: "/",
-},
-{
-    title: "Features",
-    url: "/",
-},
-{
-    title: "Pricing",
-    url: "/",
-},
-{
-    title: "Login",
-    url: "/",
-},
-{
-    title: "Signup",
-    url: "/",
-}
-]
+import AnchorLink from 'react-anchor-link-smooth-scroll';
 
 const MobileMenu = () => {
     const [navOpen, setNavOpen] = useState(false)
+    const [menu, setMenu] = useState("home")
+    const menuRef = useRef();
   return (
     <div className='block md:hidden'>
     {
@@ -36,15 +16,15 @@ const MobileMenu = () => {
         </button>
 
 <div className='absolute left-0 w-full top-20 bg-white/60 backdrop-blur-lg border-b border-t '>
-<ul className='flex flex-col items-center py-4'>
-{
-        navItems.map((item, index) => (
-            <li>
-            <a key={index} href={item.url} className='block text-gray-600 pt-4'>{item.title}</a> 
-            </li>    
-        ))
-    }
-    </ul>
+
+
+    <ul ref={menuRef} className='flex flex-col items-center py-4'>
+          <li><AnchorLink className='anchor-link' offset={50} href="#home"><p onClick={()=>setMenu("home")}>Home</p></AnchorLink>{menu==="home"?<span>_</span>:<></>}</li>
+            <li><AnchorLink className='anchor-link' offset={250} href="#work"><p onClick={()=>setMenu("about")}>How it Works</p></AnchorLink>{menu==="about"?<span>_</span>:<></>}</li>
+            <li><AnchorLink className='anchor-link' offset={100} href="#services"><p onClick={()=>setMenu("services")}>Services</p></AnchorLink>{menu==="services"?<span>_</span>:<></>}</li>
+            <li><AnchorLink className='anchor-link' offset={100} href="#testimonials"><p onClick={()=>setMenu("testimonials")}>Testimonials</p></AnchorLink>{menu==="work"?<span>_</span>:<></>}</li>
+            <li><AnchorLink className='anchor-link' offset={50} href="#contact"><p onClick={()=>setMenu("contact")}>Contact</p></AnchorLink>{menu==="contact"?<span>_</span>:<></>}</li>
+          </ul>
 </div>
 </>
         
