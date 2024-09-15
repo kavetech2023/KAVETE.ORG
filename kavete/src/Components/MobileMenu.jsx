b9d953cb695cd6a9d87cdedf8274dab4636bb1fc
kavetech2023 <kavetech@gmail.com>
2024-09-15 12:14:35 +0300

    Refactor MobileMenu component to update navigation items and adjust smooth scrolling offset

--- kavete/src/Components/MobileMenu.jsx
+++ kavete/src/Components/MobileMenu.jsx
@@ -15,16 +15,16 @@ const MobileMenu = () => {
         <X size={32} />
         </button>
 
-<div className='absolute left-0 w-full top-20 bg-white/60 backdrop-blur-lg border-b border-t '>
+<div className='absolute left-0 w-full top-8 bg-white/60 backdrop-blur-lg border-b border-t '>
 
 
-    <ul ref={menuRef} className='flex flex-col items-center py-4'>
-          <li><AnchorLink className='anchor-link' offset={50} href="#home"><p onClick={()=>setMenu("home")}>Home</p></AnchorLink>{menu==="home"?<span>_</span>:<></>}</li>
-            <li><AnchorLink className='anchor-link' offset={250} href="#work"><p onClick={()=>setMenu("about")}>How it Works</p></AnchorLink>{menu==="about"?<span>_</span>:<></>}</li>
-            <li><AnchorLink className='anchor-link' offset={100} href="#services"><p onClick={()=>setMenu("services")}>Services</p></AnchorLink>{menu==="services"?<span>_</span>:<></>}</li>
-            <li><AnchorLink className='anchor-link' offset={100} href="#testimonials"><p onClick={()=>setMenu("testimonials")}>Testimonials</p></AnchorLink>{menu==="work"?<span>_</span>:<></>}</li>
-            <li><AnchorLink className='anchor-link' offset={50} href="#contact"><p onClick={()=>setMenu("contact")}>Contact</p></AnchorLink>{menu==="contact"?<span>_</span>:<></>}</li>
-          </ul>
+    <ul ref={menuRef} className='flex flex-col items-center py-4 gap-4'>
+    <li><AnchorLink className='anchor-link' offset={100} href="#home"><p className={`${menu==="home"? "font-bold" :""}`} onClick={()=>setMenu("home")}>Home</p></AnchorLink></li>
+            <li><AnchorLink className='anchor-link' offset={250} href="#work"><p className={`${menu==="about"? "font-bold" :""}`} onClick={()=>setMenu("about")}>How it Works</p></AnchorLink></li>
+            <li><AnchorLink className='anchor-link' offset={100} href="#services"><p className={`${menu==="services"? "font-bold" :""}`} onClick={()=>setMenu("services")}>Services</p></AnchorLink></li>
+            <li><AnchorLink className='anchor-link' offset={80} href="#testimonials"><p  className={`${menu==="testimonials"? "font-bold" :""}`} onClick={()=>setMenu("testimonials")}>Testimonials</p></AnchorLink></li>
+            <li><AnchorLink className='anchor-link' offset={50} href="#contact"><p className={`${menu==="contact"? "font-bold" :""}`} onClick={()=>setMenu("contact")}>Contact</p></AnchorLink></li>
+            </ul>
 </div>
 </>
         
