6bd0641d33af3effa05006dba53c5d95e0b2362a
kavetech2023 <kavetech@gmail.com>
2024-09-21 00:10:40 +0300

    Refactor MobileMenu component to add link to dashboard

--- kavete/src/Components/MobileMenu.jsx
+++ kavete/src/Components/MobileMenu.jsx
@@ -1,6 +1,7 @@
 import React, { useRef, useState } from 'react'
 import { Menu, X } from "lucide-react";
 import AnchorLink from 'react-anchor-link-smooth-scroll';
+import { Link } from 'react-router-dom';
 
 const MobileMenu = () => {
     const [navOpen, setNavOpen] = useState(false)
@@ -24,6 +25,7 @@ const MobileMenu = () => {
             <li><AnchorLink className='anchor-link' offset={100} href="#services"><p className={`${menu==="services"? "font-bold" :""}`} onClick={()=>{setMenu("services"); setNavOpen(false)}}>Our Services</p></AnchorLink></li>
             <li><AnchorLink className='anchor-link' offset={50} href="#testimonials"><p  className={`${menu==="testimonials"? "font-bold" :""}`} onClick={()=>{setMenu("testimonials"); setNavOpen(false)}}>Testimonials</p></AnchorLink></li>
             <li><AnchorLink className='anchor-link' offset={100} href="#contact"><p className={`${menu==="pricing"? "font-bold" :""}`} onClick={()=>{setMenu("contact"); setNavOpen(false)}}>FAQs</p></AnchorLink></li>
+            <li><Link to={"/dashboard"}>Go to Jobs</Link></li>
             </ul>
 </div>
 </>
