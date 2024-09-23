13268179c9fa40264b00f3a439f590c082ee2830
kavetech2023 <kavetech@gmail.com>
2024-09-24 00:01:55 +0300

    Refactor routing in App component

--- kavete/src/App.jsx
+++ kavete/src/App.jsx
@@ -1,8 +1,7 @@
-import React, { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import Dashboard from "./Pages/Dashboard";
 import Home from "./Pages/Home";
-import SignUp from "./Pages/SignUp";
+import Edit from "./Pages/Edit";
 
 const App = () => {
 
@@ -16,7 +15,7 @@ const App = () => {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/signup" element={<SignUp />} />
+            <Route path="/edit" element={<Edit />} />
           </Routes>
         
       </div>
