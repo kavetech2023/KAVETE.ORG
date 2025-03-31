6b9c49f992565e614015fe354d87adca7c8afea8
kavetech2023 <kavetech@gmail.com>
2025-03-31 12:56:17 +0300

    remove Testimonials page

--- kavete/src/Pages/Home.jsx
+++ kavete/src/Pages/Home.jsx
@@ -1,7 +1,5 @@
 import Hero from '../Components/Hero'
-import { partnersLogo } from '../Data/partners'
 import FeaturesSection from '../Components/FeaturesSection'
-import Testimonials from '../Components/Testimonials'
 import Footer from '../Components/Footer'
 import Navbar from '../Components/Navbar'
 import FAQSection from '../Components/FAQSection'
@@ -15,7 +13,6 @@ const Home = () => {
 <Navbar/>
 <Hero />
 <FeaturesSection />
-<Testimonials />
 <FAQSection />
 <Footer />
 
